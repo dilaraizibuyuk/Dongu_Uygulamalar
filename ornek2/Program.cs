6d97da5dda0a1e24cfa38d942f3771e2c90e7d05
303DILARA_AKSAM <303DILARA_AKSAM@DESKTOP-HNE43R2>
2021-12-30 19:40:31 +0300

    Proje dosyası ekle.

---
+++ ornek2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ornek2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {     //while ile, kullanıcı adını şifreyi doğru girerse mesaj vericez yanlış girerse de hata vericez 3 kere yanlış girma hakkı var
+            int islem_sayisi = 3;
+            while (true)
+            {
+                Console.WriteLine("Kullanıcı adını giriniz:");
+                string user_name = Console.ReadLine();
+                Console.WriteLine("Şifrenizi adını giriniz:");
+                string password = Console.ReadLine();
+                if (user_name=="admin"&&password=="12345")
+                {
+                    Console.WriteLine("Tebrikler başarılı giriş yaptınız.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Kullanıcı adı veya şifre hatalı");
+                    if (islem_sayisi>0)
+                    {
+                        islem_sayisi -= 1;
+                    }
+                    if (islem_sayisi==0)
+                    {
+                        Console.WriteLine("İşlem hakkınız bitmiştir.");
+                        break;//break koymazsak tekrar kullanıcı adı ve şifre sorar
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ornek3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ornek3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //1-100 arasında random sayı ürettirme. girilen sayıyla random üretilen sayıyı karşılaştırıcaz. bulana kadar döngü dönecek
+            
+            Random rastgele = new Random();
+            int rs = 0;
+            int s;
+             rs = rastgele.Next(1, 101);
+            do
+            {
+                Console.WriteLine("Bir sayı giriniz:");
+                 s = Convert.ToInt32(Console.ReadLine());
+                if (s>rs)
+                {
+                    Console.WriteLine("Tuttuğunuz sayıyı azaltınız");
+                   
+                }
+                else if (s<rs)
+                {
+                    Console.WriteLine("Tuttuğunuz sayıyı arttırınız");
+                   
+                }
+                else
+                    Console.WriteLine("Tebrikler sayıyı buldunuz.");
+
+            } while (s!=rs);
+            Console.ReadLine();
+        }
+    }
+}
